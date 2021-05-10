aa2f0775035ee1479788555135d9dd00b1604160
Hira A <67929636+Love2Code09@users.noreply.github.com>
2021-05-09 21:32:47 -0500

    Update chat.js
    I removed the email and password values because you weren't using them for anything. The next step is to store these variables in firebase and authenticate the user.

--- chat.js
+++ chat.js
@@ -1,11 +1,11 @@
-function saveInfo() {
-	var username = document.getElementById("username").value;
-	var useremail = document.getElementByType("email").value;
-	var userpassword = document.getElementByType("password").value;
-	alert("Account creation successful! Your username is " + username + ". Now log in to chat!");
-}
 
-const signUpbtn = document.querySelector('.cta-sign-up');
-signUpBtn.addEventListener(() => {
+const signInbtn = document.querySelector('.cta-sign-in');
+const submitBtn = document.querySelector('.cta-submit');
+submitBtn.addEventListener(() => {
+	var username = document.getElementById("username").value;
+	alert("Account creation successful! Your username is " + username + ". Now log in to chat!");
+});
+
+signInBtn.addEventListener(() => {
 	window.location = "signup.html";
 });
