0a20f58a435abd1081c851c61cd674eda55c5622
Hira A <67929636+Love2Code09@users.noreply.github.com>
2021-05-09 21:28:06 -0500

    Put JS in seprate file

--- chat.js
+++ chat.js
@@ -4,3 +4,8 @@ function saveInfo() {
 	var userpassword = document.getElementByType("password").value;
 	alert("Account creation successful! Your username is " + username + ". Now log in to chat!");
 }
+
+const signUpbtn = document.querySelector('.cta-sign-up');
+signUpBtn.addEventListener(() => {
+	window.location = "signup.html";
+});
